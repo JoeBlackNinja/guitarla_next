8de1cf772db948548677f69cff36e1c8f178674e
JoeBlackNinja <jose.nava@alumnos.udg.mx>
2022-05-28 23:14:41 -0500

    193 00:00

---
+++ components/Layout.js
@@ -0,0 +1,16 @@
+import Head from "next/head";
+
+const Layout = ({children, pagina}) => {
+  return (
+    <div>
+        <Head>
+            <title>Guitar LA - {pagina}</title>
+            <meta name="description" content="Sitio Web de 
+            venta de guitarras"/>
+        </Head>
+        {children}
+    </div>    
+  )
+}
+
+export default Layout
