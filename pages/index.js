156e3e5b38843ee652cb6c53b21dfe561699729d
JoeBlackNinja <jose.nava@alumnos.udg.mx>
2022-06-07 17:09:14 -0500

    232 00:00

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,44 @@
 import Layout from '../components/Layout';
+import Listado from '../components/Listado';
+import Curso from '../components/Curso';
 
-export default function Home() {
+export default function Home({guitarras, cursos}) {
   return (
     <Layout
         pagina='Inicio'
     >
-        <h1>Desde Inicio</h1>
+        <main className='contenedor'>
+          <h1 className='heading'>Nuestra colección</h1>
+          <Listado
+            guitarrasInfo={guitarras}
+          />
+        </main>
+        <Curso
+          curso={cursos}
+        />
     </Layout>
   )
+}
+
+export async function getServerSideProps() {
+
+  const urlGuitarras = `${process.env.API_URL}/guitarras`;
+  const urlCursos = `${process.env.API_URL}/cursos`;
+
+  const [resGuitarra, resCursos] = await Promise.all([
+    fetch(urlGuitarras),
+    fetch(urlCursos)
+  ])
+
+  const [guitarras, cursos] = await Promise.all([
+    resGuitarra.json(),
+    resCursos.json()
+  ])
+
+  return {
+    props :{
+      guitarras,
+      cursos
+    }
+  }
 }
